bc20b09e98db87aa9c97cfc9f27d98c7381bb129
Maryan Serhii <sergijmara210@gmail.com>
2026-01-30 14:27:40 +0200

    fix

--- index.js
+++ index.js
@@ -24,3 +24,35 @@ document.addEventListener("click", () => {
 selectBar.forEach((bar) => {
   bar.addEventListener("click", (event) => event.stopPropagation());
 });
+
+const swiper = new Swiper(".products-swiper", {
+  slidesPerView: 4,
+  spaceBetween: 42,
+  loop: true,
+  freeMode: true,
+  navigation: {
+    nextEl: ".arrow-svg-sliser.right",
+    prevEl: ".arrow-svg-sliser.left",
+  },
+  breakpoints: {
+    320: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1200: { slidesPerView: 4 },
+  },
+});
+
+const categoryButton = document.querySelectorAll(".choose-products");
+
+categoryButton.forEach((button) => {
+  button.addEventListener("click", (Event) => {
+    const category = button.dataset.category;
+    fetchDataFurnoture(category);
+  });
+});
+
+async function fetchDataFurnoture(category) {
+  const url = `https://api.pexels.com/v1/search?query=${category}&per_page=12`;
+  const getData = await fetch(url);
+  const data = await getData.json();
+  console.log(data);
+}
