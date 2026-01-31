313a31f2031e20fd32cbdda4ac3dd263ec9edea9
Maryan Serhii <sergijmara210@gmail.com>
2026-01-31 03:01:20 +0200

    finish

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ console.log("hello");
 
 const selectButton = document.querySelectorAll(".info-dot-button");
 const selectBar = document.querySelectorAll(".hint");
+const swiperWrapper = document.querySelector(".swiper-wrapper");
 
 selectButton.forEach((button) => {
   button.addEventListener("click", (event) => {
@@ -16,9 +17,7 @@ selectButton.forEach((button) => {
 });
 
 document.addEventListener("click", () => {
-  selectBar.forEach((bar) => {
-    bar.classList.add("hidden");
-  });
+  selectBar.forEach((bar) => bar.classList.add("hidden"));
 });
 
 selectBar.forEach((bar) => {
@@ -35,24 +34,103 @@ const swiper = new Swiper(".products-swiper", {
     prevEl: ".arrow-svg-sliser.left",
   },
   breakpoints: {
-    320: { slidesPerView: 1 },
-    768: { slidesPerView: 2 },
-    1200: { slidesPerView: 4 },
+    319: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
   },
 });
 
 const categoryButton = document.querySelectorAll(".choose-products");
 
 categoryButton.forEach((button) => {
-  button.addEventListener("click", (Event) => {
+  button.addEventListener("click", async () => {
     const category = button.dataset.category;
-    fetchDataFurnoture(category);
+    try {
+      const cards = await fetchDataFurniture(category);
+      showCard(cards, category);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   });
 });
 
-async function fetchDataFurnoture(category) {
+async function fetchDataFurniture(category) {
   const url = `https://api.pexels.com/v1/search?query=${category}&per_page=12`;
-  const getData = await fetch(url);
-  const data = await getData.json();
-  console.log(data);
+  const response = await fetch(url, {
+    headers: {
+      Authorization: "HXvKfPYK3xsNI7PRMm7TPvg0KVuhcWemuQ0nTsw1cFdjn4PcFaJ29QQg",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data.photos;
 }
+
+function showCard(cards, category) {
+  swiperWrapper.innerHTML = "";
+
+  const murkup = cards
+    .map(
+      (card) => `
+    <div class="praduct-card swiper-slide">
+      <a href="#product-page" class="product-link"></a>
+      <div class="div-image">
+        <img class="img-section-product" src="${card.src.landscape}" alt="${card.alt}">
+      </div>
+      <div class="div-description">
+        <p class="card-text">${category}</p>
+        <h3 class="product-name">${card.alt}</h3>
+        <div class="stars-container">
+          <svg class="arrow-svg" width="18" height="18"><use href="./images/icons.svg#icon-Star-1"></use></svg>
+          <svg class="arrow-svg" width="18" height="18"><use href="./images/icons.svg#icon-Star-1"></use></svg>
+          <svg class="arrow-svg" width="18" height="18"><use href="./images/icons.svg#icon-Star-1"></use></svg>
+          <svg class="arrow-svg" width="18" height="18"><use href="./images/icons.svg#icon-Star-1"></use></svg>
+          <svg class="arrow-svg" width="18" height="18"><use href="./images/icons.svg#icon-Star-1"></use></svg>
+        </div>
+        <div class="wrapper-card-prise-button">
+          <p class="prise"><span class="currency">$</span>${Math.floor(card.id / 10000)}</p>
+          <button class="button-card-plus">
+            <svg class="button-plus" width="24" height="24">
+              <use href="./images/icons.svg#icon-bx_bx-plus"></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  `,
+    )
+    .join("");
+
+  swiperWrapper.insertAdjacentHTML("afterbegin", murkup);
+
+  swiper.update();
+}
+
+const openMobileButtom = document.querySelector(".open-mobile-menu");
+const closeMobileButtom = document.querySelector(".close-mobile-menu");
+const containerMobileMenu = document.querySelector(".container-mobile-menu");
+
+openMobileButtom.addEventListener("click", () => {
+  containerMobileMenu.classList.remove("hidden");
+});
+
+closeMobileButtom.addEventListener("click", () => {
+  containerMobileMenu.classList.add("hidden");
+});
